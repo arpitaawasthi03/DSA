76a7da275158265d6b79e5743b65ebe2a6229356
Arpita Awasthi <awasthi.arpita11@gmail.com>
2026-09-07 12:27:40 +0530

    Time: 0 ms (100%), Space: 18.5 MB (73.34%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,25 +11,34 @@
  */
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(root == NULL) return root;
-        TreeNode* temp = invertTree(root->left);
-        root->left = invertTree(root->right);
-        root->right = temp;
-        return root;
-    };
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr || q == nullptr ) return (p==q);
-        if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right) && p->val == q->val) return true;
-        else return false;
+    // TreeNode* invertTree(TreeNode* root) {
+    //     if(root == NULL) return root;
+    //     TreeNode* temp = invertTree(root->left);
+    //     root->left = invertTree(root->right);
+    //     root->right = temp;
+    //     return root;
+    // };
+    // bool isSameTree(TreeNode* p, TreeNode* q) {
+    //     if(p == nullptr || q == nullptr ) return (p==q);
+    //     if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right) && p->val == q->val) return true;
+    //     else return false;
         
+    // }
+    bool isInvert(TreeNode* p, TreeNode* q){
+        if(p== NULL && q == NULL) return true;
+        if(p== NULL || q == NULL) return false;
+        if(p->val != q->val) return false;
+        return isInvert(p->left,q->right) && isInvert(p->right, q->left);
     }
     bool isSymmetric(TreeNode* root) {
         // return isSameTree(invertTree(root->left),root->right);
-        invertTree(root->right);
-        bool ans = isSameTree(root->left,root->right);
-        invertTree(root->right);
-        return ans;
 
+
+        // invertTree(root->right);
+        // bool ans = isSameTree(root->left,root->right);
+        // invertTree(root->right);
+        // return ans;
+
+        return isInvert(root->left, root->right);
     }
 };
